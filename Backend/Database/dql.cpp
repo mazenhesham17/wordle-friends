c93633a2fa4c1b41cb31244b878deaa5073934ff
mazenhesham17 <mazenhesham7@live.com>
2024-02-06 19:38:21 +0200

    .

---
+++ Backend/Database/dql.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mazen on 2/6/24.
+//
+
+#include "dql.h"
---
+++ Backend/Database/dql.h
@@ -0,0 +1,8 @@
+//
+// Created by mazen on 2/6/24.
+//
+
+#ifndef BACKEND_DQL_H
+#define BACKEND_DQL_H
+
+#endif //BACKEND_DQL_H
